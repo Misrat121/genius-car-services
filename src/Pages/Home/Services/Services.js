a86ae9f9862493299030119667ce8c656ae79af0
Misrat121 <you@example.com>
2022-04-11 22:32:38 +0600

    json file created

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
 
 const Services = () => {
+
+    const [] = useState([])
+
     return (
         <div>
             <h2>service</h2>
