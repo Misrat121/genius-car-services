ca9df2141e8cc62a75a90676a47d380458b9e9f9
Misrat121 <you@example.com>
2022-04-14 13:19:04 +0600

    email password authentication with firebase hooks

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,30 +1,44 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
 
-    const handleSubmit = event =>{
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
 
-        console.log(email, password)
-    }
+  if (user) {
+    navigate("/home");
+  }
 
-    const navigateRegister = event => {
-        navigate('/register')
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
   return (
     <div className="container w-50 mx-auto">
       <h2 className="text-primary text-center mt-4">Please Login</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -32,7 +46,12 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -42,8 +61,16 @@ const Login = () => {
         </Button>
       </Form>
 
-     <p>New to Genius Car? <Link to="/register" className="text-danger text-decoration-none pe-auto" onClick={navigateRegister}>Please Register</Link></p>
-
+      <p>
+        New to Genius Car?{" "}
+        <Link
+          to="/register"
+          className="text-danger text-decoration-none pe-auto"
+          onClick={navigateRegister}
+        >
+          Please Register
+        </Link>
+      </p>
     </div>
   );
 };
